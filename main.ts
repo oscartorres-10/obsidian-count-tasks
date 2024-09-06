ee9fa27a3400ec00c4cbe7bc21e58ab2cd253474
oscartorres <38195378+oscartorres-10@users.noreply.github.com>
2024-09-06 16:01:44 -0300

    style(main.ts): delete unused, formatting

--- main.ts
+++ main.ts
@@ -1,6 +1,4 @@
-import { App, Editor, MarkdownView, Plugin, PluginSettingTab, Setting } from 'obsidian';
-
-// Remember to rename these classes and interfaces!
+import { App, Plugin, PluginSettingTab, Setting } from 'obsidian';
 
 interface CountTasksSettings {
 	mySetting: string;
@@ -16,9 +14,8 @@ export default class CountTasksPlugin extends Plugin {
 
 	async onload() {
 		console.log('loading count tasks plugin');
-
 		await this.loadSettings();
-		// This adds a status bar item to the bottom of the app. Does not work on mobile apps.
+		// adds a status bar item to the bottom of the app. Does not work on mobile apps.
 		const statusBarTasksCount = this.addStatusBarItem();
 
 		this.app.workspace.on('file-open', async () => {
@@ -26,37 +23,14 @@ export default class CountTasksPlugin extends Plugin {
 			const activeFile = this.app.workspace.getActiveFile();
 			if (editor && activeFile) {
 				const cachedRead = await this.app.vault.cachedRead(activeFile);
-				console.log('cachedRead ->', cachedRead);
 				const matchResults = cachedRead.match(/- \[ \]/g);
-				console.log('matchResults ->', matchResults);
 				this.tasksCount = matchResults?.length || 0;
 			}
 			statusBarTasksCount.setText(`Tasks: ${this.tasksCount}`);
 		});
 
-		// This adds an editor command that can perform some operation on the current editor instance
-		this.addCommand({
-			id: 'sample-editor-command',
-			name: 'Sample editor command',
-			editorCallback: (editor: Editor, view: MarkdownView) => {
-				console.log(editor.getSelection());
-				editor.replaceSelection('Sample Editor Command');
-			},
-		});
-
-		// This adds a settings tab so the user can configure various aspects of the plugin
+		// adds a settings tab so the user can configure various aspects of the plugin
 		this.addSettingTab(new SampleSettingTab(this.app, this));
-
-		// If the plugin hooks up any global DOM events (on parts of the app that doesn't belong to this plugin)
-		// Using this function will automatically remove the event listener when this plugin is disabled.
-		this.registerDomEvent(document, 'click', (evt: MouseEvent) => {
-			console.log('click', evt);
-		});
-
-		// When registering intervals, this function will automatically clear the interval when the plugin is disabled.
-		this.registerInterval(
-			window.setInterval(() => console.log('setInterval'), 5 * 60 * 1000)
-		);
 	}
 
 	onunload() {
@@ -85,19 +59,21 @@ class SampleSettingTab extends PluginSettingTab {
 	}
 
 	display(): void {
-		const {containerEl} = this;
+		const { containerEl } = this;
 
 		containerEl.empty();
 
 		new Setting(containerEl)
 			.setName('Setting #1')
-			.setDesc('It\'s a secret')
-			.addText(text => text
-				.setPlaceholder('Enter your secret')
-				.setValue(this.plugin.settings.mySetting)
-				.onChange(async (value) => {
-					this.plugin.settings.mySetting = value;
-					await this.plugin.saveSettings();
-				}));
+			.setDesc("It's a secret")
+			.addText((text) =>
+				text
+					.setPlaceholder('Enter your secret')
+					.setValue(this.plugin.settings.mySetting)
+					.onChange(async (value) => {
+						this.plugin.settings.mySetting = value;
+						await this.plugin.saveSettings();
+					})
+			);
 	}
 }
